622bea63c6c8bab3c16a0fe12099e57a2d2285f9
Hilll19 <hilworking0110@gmail.com>
2023-12-10 21:09:20 +0700

    Create Navbar like menu and button burger etc

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 import styles from './featured.module.css'
 const Featured = () => {
   return (
-    <div className={styles.container}>Featured</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Welcome to MentBlog!</b> 
+        Discover my stories and creatives ideas
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}></div>
+        <div className={styles.textContainer}></div>
+      </div>
+    </div>
   )
 }
 
