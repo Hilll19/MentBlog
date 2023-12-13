23ff29890e105af190f49eb967f9700bc99a5e7b
Hilll19 <hilworking0110@gmail.com>
2023-12-14 01:53:32 +0700

    Setting backend for auth account

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,22 +1,23 @@
 "use client"
 import React, { useState } from 'react';
 import styles from './authLinks.module.css';
+import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 
 const AuthLinks = () => {
 
   const [open, setOpen] = useState(false);
   //temporary check status
-  const status = "authenticated";
+  const {status} = useSession();
 
   return (
     <>
-      {status === "notauthenticated" ? (
+      {status === "unauthenticated" ? (
         <Link href="/Login" className={styles.link}>Login</Link>
       ) : (
         <>
           <Link href="/write" className={styles.link}>Write</Link>
-          <span className={styles.link}>Logout</span>
+          <span className={styles.link} onClick={signOut}>Logout</span>
         </>
       )}
       <div className={styles.burger} onClick={()=> setOpen(!open)}>
@@ -29,7 +30,7 @@ const AuthLinks = () => {
           <Link href="/"> Homepage</Link>
           <Link href="/"> About</Link>
           <Link href="/"> Contact</Link>
-          {status === "notauthenticated" ? (
+          {status === "unauthenticated" ? (
             <Link href="/Login">Login</Link>
           ) : (
             <>
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,11 @@
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./connect";
+// import { Prisma } from "@prisma/client";
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma) ,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
