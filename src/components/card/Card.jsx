b02c0c66f5e22e4b2abba20a5745ab392614b100
Hilll19 <hilworking0110@gmail.com>
2023-12-14 23:41:47 +0700

    Single page and connect with use OAuth

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -7,18 +7,18 @@ const Card = ({key, item}) => {
     return (
         <div className={styles.container} key={key}>
             <div className={styles.imageContainer}>
-                <Image src="/p1.jpeg" alt='' fill className={styles.image} />
+                {item.img && (<Image src={item.img} alt='' fill className={styles.image} />)}
             </div>
             <div className={styles.textContainer}>
                 <div className={styles.detail}>
-                    <span className={styles.date}>11.02.2023 - </span>
-                    <span className={styles.category}>CULTURE</span>
+                    <span className={styles.date}>{item.createdAt.substring(0, 10)} - </span>
+                    <span className={styles.category}>{item.catSlug}</span>
                 </div>
-                <Link href="/">
+                <Link href={`/posts/${item.slug}`}>
                     <h1>{item.title}</h1>
                 </Link>
-                <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat iusto, similique, quae, ea totam placeat fugiat corporis aspernatur officia quasi sapiente! Laudantium illo alias magni labore sapiente, est recusandae beatae.</p>
-                <Link href="/" className={styles.link}>Read More</Link>
+                <p className={styles.desc}>{item.desc.substring(0, 60)}</p>
+                <Link href={`/posts/${item.slug}`} className={styles.link}>Read More</Link>
             </div>
         </div>
     )
